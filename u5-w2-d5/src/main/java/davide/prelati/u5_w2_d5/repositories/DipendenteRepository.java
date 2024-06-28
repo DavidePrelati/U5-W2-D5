ffc061ad88ffef697f15751fee5695bad43da495
DavidePrelati <davide.prelati@gmail.com>
2024-06-28 15:25:32 +0200

    creazione repo

---
+++ u5-w2-d5/src/main/java/davide/prelati/u5_w2_d5/repositories/DipendenteRepository.java
@@ -0,0 +1,7 @@
+package davide.prelati.u5_w2_d5.repositories;
+
+import davide.prelati.u5_w2_d5.entities.Dipendente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DipendenteRepository extends JpaRepository<Dipendente, Long> {
+}
---
+++ u5-w2-d5/src/main/java/davide/prelati/u5_w2_d5/repositories/DispositivoRepository.java
@@ -0,0 +1,7 @@
+package davide.prelati.u5_w2_d5.repositories;
+
+import davide.prelati.u5_w2_d5.entities.Dispositivo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DispositivoRepository extends JpaRepository<Dispositivo, Long> {
+}
